84182818ab5a1fe39a8d64af981d885bed0e9297
Matthew Hebert <soyrux@gmail.com>
2015-01-03 18:10:23 -0500

    Initial commit

---
+++ src/portbuster/Client.java
@@ -0,0 +1,113 @@
+package portbuster;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        startServer("127.0.0.1");
+    }
+
+    public static void startServer(final String ip) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Scanner scanner = new Scanner(System.in);
+
+                while(true) {
+                    String line = scanner.nextLine();
+
+                    String[] spl = line.split("\\s+");
+
+                    if(line.equalsIgnoreCase("quit")) {
+                        System.exit(0);
+                    }
+                }
+            }
+        }).start();
+
+        try {
+            // Create socket listener
+            final Socket socket = new Socket(ip, 25564);
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+
+                        while(true) {
+                            String s = dataInputStream.readLine();
+//                            System.out.println("Client connected: play.daexsys.com:" + s);
+
+                            // Per client socket
+                            final Socket local = new Socket(ip, 25563);
+                            final Socket remote = new Socket("127.0.0.1", 25565);
+
+                            new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+//                                    while(true) {
+                                        byte[] buffer = new byte[4096];
+                                        int size = 0;
+
+                                        try {
+                                            while ((size = local.getInputStream().read(buffer)) != -1) {
+                                                remote.getOutputStream().write(buffer, 0, size);
+                                            }
+                                        } catch (IOException e) {
+                                            try {
+                                                Thread.sleep(100000000);
+                                            } catch (InterruptedException e1) {
+                                                e1.printStackTrace();
+                                            }
+                                            try {
+                                                local.close();
+                                            } catch (IOException e1) {
+                                                e1.printStackTrace();
+                                            }
+                                            e.printStackTrace();
+                                        }
+//                                    }
+                                }
+                            }).start();
+
+                            new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+//                                    while(true) {
+                                        byte[] buffer = new byte[4096];
+                                        int size = 0;
+
+                                        try {
+                                            while ((size = remote.getInputStream().read(buffer)) != -1) {
+                                                local.getOutputStream().write(buffer, 0, size);
+                                            }
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                            try {
+                                                Thread.sleep(100000000);
+                                            } catch (InterruptedException e1) {
+                                                e1.printStackTrace();
+                                            }
+                                            try {
+                                                remote.close();
+                                            } catch (IOException e1) {
+                                                e1.printStackTrace();
+                                            }
+//                                        }
+                                    }
+                                }
+                            }).start();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/portbuster/Server.java
@@ -0,0 +1,100 @@
+package portbuster;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+    public static int highestPossible = 25566;
+    public static void main(String[] args) {
+        startServer();
+    }
+
+    public static void startServer() {    new Thread(new Runnable() {
+        @Override
+        public void run() {
+            Scanner scanner = new Scanner(System.in);
+
+            while(true) {
+                String line = scanner.nextLine();
+
+                String[] spl = line.split("\\s+");
+
+                if(line.equalsIgnoreCase("quit")) {
+                    System.exit(0);
+                }
+            }
+        }
+    }).start();
+        try {
+            ServerSocket trafficSocket  = new ServerSocket(25563);
+            ServerSocket messageSocket  = new ServerSocket(25564);
+
+            while(true) {
+                int port = highestPossible;
+                Socket theListener = messageSocket.accept();
+
+                ServerSocket externalSocket = new ServerSocket(highestPossible);
+                System.out.println("Waiting for connections at port: "+highestPossible);
+                highestPossible++;
+
+
+                while(true) {
+                    final Socket local = externalSocket.accept();
+                    System.out.println("Client connected");
+
+                    PrintStream printStream = new PrintStream(theListener.getOutputStream());
+                    printStream.println(port);
+
+                    // Get traffic socket.
+                    final Socket remote = trafficSocket.accept();;
+
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                while (true) {
+                                    byte[] buffer = new byte[4096];
+                                    int size = 0;
+
+                                    while ((size = local.getInputStream().read(buffer)) != -1) {
+                                        remote.getOutputStream().write(buffer, 0, size);
+                                    }
+                                }
+                            } catch (Exception e) {
+                                try {
+                                    local.close();
+                                } catch (IOException e1) {
+                                }
+                            }
+                        }
+                    }).start();
+
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                while (true) {
+                                    byte[] buffer = new byte[4096];
+                                    int size = 0;
+
+                                    while ((size = remote.getInputStream().read(buffer)) != -1) {
+                                        local.getOutputStream().write(buffer, 0, size);
+                                    }
+                                }
+                            } catch (Exception e) {
+                                try {
+                                    remote.close();
+                                } catch (IOException e1) {
+                                }
+                            }
+                        }
+                    }).start();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
