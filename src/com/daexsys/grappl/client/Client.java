553784e43ccf5c65cc93f3c00a7ed01d643d48c7
Matthew Hebert <soyrux@gmail.com>
2015-06-06 10:57:32 -0400

    Client changes, +heartbeat

--- src/com/daexsys/grappl/client/Client.java
+++ src/com/daexsys/grappl/client/Client.java
@@ -1,6 +1,6 @@
-package com.daexsys.moxc.portbuster;
+package com.daexsys.grappl.client;
 
-import com.daexsys.moxc.GrapplGlobal;
+import com.daexsys.grappl.GrapplGlobal;
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,6 +13,11 @@ import java.util.Scanner;
 public class Client {
     public static JFrame jFrame;
     public static JLabel jLabel3;
+    public static volatile JLabel jLabel4;
+    public static int sent = 0;
+    public static int recv = 0;
+
+    public static int connectedClients = 0;
 
     public static void main(String[] args) {
         boolean displayGui = true;
@@ -31,10 +36,23 @@ public class Client {
 
     public static void proceed(final String ip, int port, final boolean displayGui) {
         if(displayGui) {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (UnsupportedLookAndFeelException e) {
+                e.printStackTrace();
+            }
+
             jFrame = new JFrame(GrapplGlobal.APP_NAME + " Client");
-            jFrame.setSize(new Dimension(300, 220));
+            jFrame.setSize(new Dimension(300, 240));
             jFrame.setVisible(true);
             jFrame.setLayout(null);
+            jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
             JButton jButton = new JButton("Close " + GrapplGlobal.APP_NAME + " Client");
             jButton.addActionListener(new ActionListener() {
@@ -44,7 +62,7 @@ public class Client {
                 }
             });
             jFrame.add(jButton);
-            jButton.setBounds(0, 75, 280, 100);
+            jButton.setBounds(0, 95, 280, 100);
         }
 
         if(displayGui) {
@@ -73,32 +91,85 @@ public class Client {
 
         try {
             // Create socket listener
-            final Socket messageSocket = new Socket(ip, GrapplGlobal.SPAWN_PORT);
-            new PrintStream(messageSocket.getOutputStream()).println(SERVICE_PORT + "");
+            final Socket messageSocket = new Socket(ip, GrapplGlobal.MESSAGE_PORT);
 
             final DataInputStream messageInputStream = new DataInputStream(messageSocket.getInputStream());
             String s = messageInputStream.readLine();
             System.out.println(GrapplGlobal.DOMAIN + ":" + s);
 
             if(displayGui) {
-                final JLabel jLabel = new JLabel("GLOBAL ADDRESS: " + GrapplGlobal.DOMAIN + ":" + s);
+                final JLabel jLabel = new JLabel("Global Address: " + GrapplGlobal.DOMAIN + ":" + s);
                 jLabel.setBounds(5, 5, 450, 20);
                 jFrame.add(jLabel);
 
-                JLabel jLabel2 = new JLabel("SERVER ON LOCAL PORT: " + SERVICE_PORT);
+                JLabel jLabel2 = new JLabel("Server on local port: " + SERVICE_PORT);
                 jLabel2.setBounds(5, 25, 450, 20);
                 jFrame.add(jLabel2);
+
+                jLabel4 = new JLabel("Waiting for data");
+                jLabel4.setBounds(5, 65, 450, 20);
+                jFrame.add(jLabel4);
+
                 jFrame.repaint();
+            }
+
+            if(displayGui) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (true) {
+                            if (jLabel4 != null && jLabel3 != null) {
+                                jLabel3.setText("Connected clients: " + connectedClients);
+                                jLabel4.setText("Sent Data: " + (sent*4) + "KB - Recv Data: " + (recv*4)+"KB");
+                                jFrame.repaint();
+                            }
+
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }).start();
             }
 
             new Thread(new Runnable() {
                 @Override
                 public void run() {
+                    Socket heartBeat = null;
+                    DataOutputStream dataOutputStream = null;
+
                     try {
-                        int connectedClients = 0;
+                        heartBeat = new Socket(GrapplGlobal.DOMAIN, GrapplGlobal.HEARTBEAT);
+                        dataOutputStream = new DataOutputStream(heartBeat.getOutputStream());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    while(true) {
+                        try {
+                            dataOutputStream.writeInt(0);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
 
                         if(displayGui) {
-                            jLabel3 = new JLabel("CONNECTED CLIENTS: " + connectedClients);
+                            jLabel3 = new JLabel("Connected clients: " + connectedClients);
                             jLabel3.setBounds(5, 45, 450, 20);
                             jFrame.add(jLabel3);
                             jFrame.repaint();
@@ -111,10 +182,6 @@ public class Client {
 
                             // Increment the connected player counter.
                             connectedClients++;
-                            if(displayGui) {
-                                jLabel3.setText("CONNECTED CLIENTS: " + connectedClients);
-                                jFrame.repaint();
-                            }
 
                             // This socket connects to the local server.
                             final Socket toLocal = new Socket("127.0.0.1", SERVICE_PORT);
@@ -131,13 +198,14 @@ public class Client {
                                     try {
                                         while ((size = toLocal.getInputStream().read(buffer)) != -1) {
                                             toRemote.getOutputStream().write(buffer, 0, size);
+                                            sent += 1;
                                         }
                                     } catch (IOException e) {
                                         try {
                                             toLocal.close();
                                             toRemote.close();
                                         } catch (IOException e1) {
-//                                            e1.printStackTrace();
+                                            e1.printStackTrace();
                                         }
 //                                        e.printStackTrace();
                                     }
@@ -162,6 +230,7 @@ public class Client {
                                     try {
                                         while ((size = toRemote.getInputStream().read(buffer)) != -1) {
                                             toLocal.getOutputStream().write(buffer, 0, size);
+                                            recv += 1;
                                         }
                                     } catch (IOException e) {
                                         e.printStackTrace();
@@ -177,13 +246,18 @@ public class Client {
                                         toLocal.close();
                                         toRemote.close();
                                     } catch (IOException e) {
-//                                        e.printStackTrace();
+                                        e.printStackTrace();
                                     }
                                 }
                             });
                             remoteToLocal.start();
                         }
                     } catch (IOException e) {
+                        try {
+                            messageSocket.close();
+                        } catch (IOException e1) {
+                            e1.printStackTrace();
+                        }
                         e.printStackTrace();
                     }
                 }
